3c0fae9b0a5c663185585765c284a5b671777b21
Sidharthkp <sidharthkannan20@gmail.com>
2023-01-18 17:46:56 +0530

    Commit home page navbar

--- src/components/Navbar/User_side/Navbar.tsx
+++ src/components/Navbar/User_side/Navbar.tsx
@@ -1,41 +1,92 @@
-const Navbar_user = () => {
+import { useState } from "react";
+import { FaMicrosoft, FaUserAltSlash, FaUserAstronaut, FaUserCircle, FaUserSecret } from "react-icons/fa";
+
+export default function NavBar() {
+    const [navbar, setNavbar] = useState(false);
+
     return (
-        <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
-            <div className="container flex flex-wrap items-center justify-between mx-auto">
-                <a href="#" className="flex items-center">
-                    <img src="..\src\assets\Logo\2.png" className="h-6 mr-3 sm:h-9" alt="CrowdCruisers" />
-                    <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Crowd Cruisers</span>
-                </a>
-                <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-                    <span className="sr-only">Open main menu</span>
-                    <div className="space-y-1">
-                        <span className="block w-6 h-1 bg-gray-600"></span>
-                        <span className="block w-6 h-1 bg-gray-600"></span>
-                        <span className="block w-6 h-1 bg-gray-600"></span>
+        <nav className="w-full bg-gray-700 shadow">
+            <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex lg:flex xl:flex 2xl:flex lg:px-8 xl:px-8 md:px-8">
+                <div>
+                    <div className="flex items-center justify-between py-3 md:py-5 lg:py-5 xl:py-5 2xl:py-5 md:block lg:block xl:block 2xl:block">
+                        <a href="javascript:void(0)">
+                            <h2 className="text-2xl font-bold text-white">Crowd Cruisers</h2>
+                        </a>
+                        <div className="md:hidden xl:hidden 2xl:hidden lg:hidden">
+                            <button
+                                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                                onClick={() => setNavbar(!navbar)}
+                            >
+                                {navbar ? (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-6 h-6 text-white"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                            clipRule="evenodd"
+                                        />
+                                    </svg>
+                                ) : (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-6 h-6 text-white"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        strokeWidth={2}
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M4 6h16M4 12h16M4 18h16"
+                                        />
+                                    </svg>
+                                )}
+                            </button>
+                        </div>
                     </div>
-                </button>
-                <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                        <li>
-                            <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
-                        </li>
-                        <li>
-                            <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
-                        </li>
-                        <li>
-                            <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-                        </li>
-                        <li>
-                            <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-                        </li>
-                        <li>
-                            <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                </div>
+                <div>
+                    <div
+                        className={`flex-1 justify-self-center pb-3 mt-6 md:block lg:block xl:block md:pb-0 md:mt-0 ${navbar ? "block" : "hidden"
+                            }`}
+                    >
+                        <ul className="items-center justify-center space-y-8 md:flex xl:flex lg:flex lg:space-x-6 lg:space-y-0 xl:space-x-6 xl:space-y-0 md:space-x-6 md:space-y-0">
+                            <li className="text-white hover:text-indigo-200">
+                                <a href="javascript:void(0)">Home</a>
+                            </li>
+                            <li className="text-white hover:text-indigo-200">
+                                <a href="javascript:void(0)">Map</a>
+                            </li>
+                            <li className="text-white hover:text-indigo-200">
+                                <a href="javascript:void(0)">Community</a>
+                            </li>
+                            <li className="text-white hover:text-indigo-200">
+                                <a href="javascript:void(0)">Rides</a>
+                            </li>
+                            <li className="text-white hover:text-indigo-200">
+                                <a href="javascript:void(0)">Events</a>
+                            </li>
+                            <li className="text-white hover:text-indigo-200">
+                                <a href="javascript:void(0)">Apparel Store</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="sm:hidden space-x-2 lg:inline-block md:inline-block xl:inline-block">
+                    <ul className="items-center justify-center space-y-8 md:flex xl:flex lg:flex lg:space-x-6 lg:space-y-0 xl:space-x-6 xl:space-y-0 md:space-x-6 md:space-y-0">
+                        <li className="text-white hover:text-indigo-200 mt-5">
+                            <a href="#">
+                                <FaUserCircle className='text-2xl' />
+                            </a>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
-    )
+    );
 }
-
-export default Navbar_user
