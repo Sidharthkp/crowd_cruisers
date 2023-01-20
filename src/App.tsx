dde46fc4d048c12412926b8165353c4f55fc4e88
Sidharthkp <sidharthkannan20@gmail.com>
2023-01-20 14:18:12 +0530

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import Login from "./components/Authentication/Login"
 import Signup from "./components/Authentication/Signup"
 import Home from "./components/Pages/Home"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { useSelector } from "react-redux"
+import { useState } from "react"
 const App = () => {
+    const [isAuth, setIsAuth] = useState(false);
+    const authorized = useSelector((state: any) => state.authorizer.authorized);
     return (
         <div>
             <BrowserRouter>
                 <Routes>
+                    {authorized}
                     <Route path='/signup' element={<Signup />} />
                     <Route path='/login' element={<Login />} />
                     <Route path='/' element={<Home />} />
