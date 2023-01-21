d55d4c750f2fdf91f54ac3583b7fefeea812ebf7
Sidharthkp <sidharthkannan20@gmail.com>
2023-01-21 14:13:14 +0530

    Commit

--- src/components/Navbar/User_side/Navbar.tsx
+++ src/components/Navbar/User_side/Navbar.tsx
@@ -161,22 +161,22 @@ const NavBar = () => {
                                             <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                                                 <Menu.Item>
                                                     {({ active }) => (
-                                                        <a
+                                                        <button
                                                         onClick={login}
-                                                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                                            className={classNames(active ? 'bg-gray-100 cursor-pointer w-full' : '', 'block px-4 py-2 w-full text-sm text-gray-700')}
                                                         >
                                                             Sign In
-                                                        </a>
+                                                        </button>
                                                     )}
                                                 </Menu.Item>
                                                 <Menu.Item>
                                                     {({ active }) => (
-                                                        <a
+                                                        <button
                                                             onClick={signup}
-                                                            className={classNames(active ? 'bg-gray-100 cursor-pointer' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                                            className={classNames(active ? 'bg-gray-100 cursor-pointer w-full' : '', 'block px-4 py-2 text-sm w-full text-gray-700')}
                                                         >
                                                             Sign Up
-                                                        </a>
+                                                        </button>
                                                     )}
                                                 </Menu.Item>
                                             </Menu.Items>
