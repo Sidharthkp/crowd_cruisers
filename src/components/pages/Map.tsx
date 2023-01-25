6edca56156413428fecf3b6df6d8efad3bbaf58d
Sidharthkp <sidharthkannan20@gmail.com>
2023-01-25 21:25:48 +0530

    There is one bug

--- src/components/pages/Map.tsx
+++ src/components/pages/Map.tsx
@@ -18,6 +18,8 @@ const MapPage = () => {
         zoom: 3
     })
     const [currentPlaceId, setCurrentPlaceId] = React.useState(null);
+    const [newTitle, setNewTitle] = React.useState("");
+    const [newDescription, setNewDescription] = React.useState("");
     const [newPlace, setNewPlace] = React.useState<Place | null>(null);
     React.useEffect(() => {
         const getPins = async () => {
@@ -43,6 +45,26 @@ const MapPage = () => {
             latitude,
             longitude
         })
+    }
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        const newPin = {
+            username: currentUser,
+            title: newTitle,
+            description: newDescription,
+            latitude: newPlace?.latitude,
+            longitude: newPlace?.longitude
+        }
+
+        try {
+            const res = await axios.post("http://localhost:3000/api/pins", newPin)
+            setPins(res.data);
+            setNewPlace(null);
+        } catch (err) {
+            console.log(err);
+
+        }
     }
 
     return (
@@ -85,18 +107,18 @@ const MapPage = () => {
 
                                     <div className="max-w-sm rounded overflow-hidden shadow-lg text-black">
                                         <div className="w-full max-w-xs">
-                                            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                                            <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                                                 <div className="mb-4">
                                                     <label className="block text-gray-700 text-sm font-bold mb-2">
                                                         Title
                                                     </label>
-                                                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Title" />
+                                                    <input onChange={(e) => setNewTitle(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Title" />
                                                 </div>
                                                 <div className="mb-4">
                                                     <label className="block text-gray-700 text-sm font-bold mb-2">
                                                         Description
                                                     </label>
-                                                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Tell me more about it" />
+                                                    <input onChange={(e) => setNewDescription(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Tell me more about it" />
                                                 </div>
                                                 <div className='w-full flex justify-end'>
                                                     <button className='rounded-2xl bg-green-400 w-14 h-6 text-white' type='submit'>Share</button>
