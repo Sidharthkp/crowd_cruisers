bf90c19a57f82ccec8fe08244ec5966aa9acd0ff
Sidharthkp <sidharthkannan20@gmail.com>
2023-01-25 19:55:42 +0530

    working on map

--- src/components/pages/Map.tsx
+++ src/components/pages/Map.tsx
@@ -6,9 +6,14 @@ import axios from 'axios'
 import { format } from 'timeago.js';
 
 const MapPage = () => {
+    interface Place {
+        longitude: number;
+        latitude: number;
+    }
+    const currentUser = "Ajmal"
     const [pins, setPins] = React.useState([]);
     const [currentPlaceId, setCurrentPlaceId] = React.useState(null);
-
+    const [newPlace, setNewPlace] = React.useState<Place | null>(null);
     React.useEffect(() => {
         const getPins = async () => {
             try {
@@ -26,6 +31,14 @@ const MapPage = () => {
         setCurrentPlaceId(id)
     }
 
+    const handleAddClick = (e: any) => {
+        const { lat: latitude, lng: longitude } = e.lngLat;
+        setNewPlace({
+            latitude,
+            longitude
+        })
+    }
+
     return (
         <>
             <Navbar_user />
@@ -39,6 +52,7 @@ const MapPage = () => {
                     mapboxAccessToken={import.meta.env.VITE_MAPBOX}
                     style={{ width: '100vw', height: '100vh' }}
                     mapStyle="mapbox://styles/mapbox/streets-v9"
+                    onDblClick={handleAddClick}
                 >
                     {pins.map((p: any) => {
                         console.log(p.longitude, "AND", p.latitude);
@@ -46,7 +60,7 @@ const MapPage = () => {
                         return (<>
 
                             <Marker longitude={p.longitude} onClick={() => handleShowPopup(p._id)} latitude={p.latitude} anchor="bottom">
-                                <FmdGoodIcon style={{ fontSize: 64, color: "tomato", cursor: "pointer" }} />
+                                <FmdGoodIcon style={{ fontSize: p.username === currentUser ? 64 : 40, color: p.username === currentUser ? "tomato" : "purple", cursor: "pointer" }} />
                             </Marker>
 
 
@@ -64,12 +78,26 @@ const MapPage = () => {
                                     </div>
 
                                 </Popup>
-                    )}
-                        </>
+                            )}
+                            {newPlace &&
+                                <Popup longitude={newPlace.longitude} latitude={newPlace.latitude} onClose={()=>setNewPlace(null)} closeOnClick={false} closeButton={true} className="text-black" anchor="bottom" >
 
+                                    <div className="max-w-sm rounded overflow-hidden shadow-lg text-black">
+                                        <div className="px-2 py-3">
+                                            {/* <div className="font-bold text-xl">{p.title}</div> */}
+                                            <p className="text-gray-700 text-base">
+                                                HEllo
+                                            </p>
+                                            {/* <div>{format(p.createdAt)}</div> */}
+                                        </div>
+                                    </div>
+
+                                </Popup>}
+                        </>
 
                         )
                     })}
+
                 </Map>
             </div>
         </>
