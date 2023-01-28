8cb7f6372c9743080571aca40413c0465ac3fe22
Sidharthkp <sidharthkannan20@gmail.com>
2023-01-28 11:25:42 +0530

    Solved a big error in map unique key

--- src/components/pages/map/Map.tsx
+++ src/components/pages/map/Map.tsx
@@ -73,69 +73,71 @@ const MapPage = () => {
         <>
             <Navbar_user />
             <div>
-                <Map
-                    initialViewState={{ ...viewPort }}
-                    mapboxAccessToken={import.meta.env.VITE_MAPBOX}
-                    style={{ width: '100vw', height: '100vh' }}
-                    mapStyle="mapbox://styles/mapbox/streets-v9"
-                    onDblClick={handleAddClick}
-                >
-                    {pins.map((p: any) => {
+                {pins.length > 0 && (
+                    <Map
+                        initialViewState={{ ...viewPort }}
+                        mapboxAccessToken={import.meta.env.VITE_MAPBOX}
+                        style={{ width: '100vw', height: '100vh' }}
+                        mapStyle="mapbox://styles/mapbox/streets-v9"
+                        onDblClick={handleAddClick}
+                    >
+                        {pins.map((p: any) => {
 
-                        return (<>
+                            return (<div key={p._id}>
 
-                            <Marker key={p._id} longitude={p.longitude} latitude={p.latitude} anchor="bottom">
-                                <FmdGoodIcon key={p._id} onClick={() => handleShowPopup(p._id, p.latitude, p.longitude)} style={{ fontSize: p.username === currentUser ? 64 : 40, color: p.username === currentUser ? "tomato" : "purple", cursor: "pointer" }} />
-                            </Marker>
+                                <Marker longitude={p.longitude} latitude={p.latitude} anchor="bottom">
+                                    <FmdGoodIcon key={p._id} onClick={() => handleShowPopup(p._id, p.latitude, p.longitude)} style={{ fontSize: p.username === currentUser ? 64 : 40, color: p.username === currentUser ? "tomato" : "purple", cursor: "pointer" }} />
+                                </Marker>
 
 
-                            {p._id === currentPlaceId && (
-                                <Popup key={p._id} longitude={p.longitude} latitude={p.latitude} closeOnClick={false} closeButton={true} className="text-black" anchor="bottom" >
+                                {p._id === currentPlaceId && (
+                                    <Popup longitude={p.longitude} latitude={p.latitude} closeOnClick={false} closeButton={true} className="text-black" anchor="bottom" >
 
-                                    <div className="max-w-sm rounded overflow-hidden shadow-lg text-black">
-                                        <div className="px-2 py-3">
-                                            <div className="font-bold text-xl">{p.title}</div>
-                                            <p className="text-gray-700 text-base">
-                                                {p.description}
-                                            </p>
-                                            <div>{format(p.createdAt)}</div>
+                                        <div className="max-w-sm rounded overflow-hidden shadow-lg text-black">
+                                            <div className="px-2 py-3">
+                                                <div className="font-bold text-xl">{p.title}</div>
+                                                <p className="text-gray-700 text-base">
+                                                    {p.description}
+                                                </p>
+                                                <div>{format(p.createdAt)}</div>
+                                            </div>
                                         </div>
-                                    </div>
 
-                                </Popup>
-                            )}
-                            {newPlace &&
-                                <Popup longitude={newPlace.longitude} latitude={newPlace.latitude} onClose={() => setNewPlace(null)} closeOnClick={false} closeButton={true} className="text-black" anchor="bottom" >
+                                    </Popup>
+                                )}
+                                {newPlace &&
+                                    <Popup longitude={newPlace.longitude} latitude={newPlace.latitude} onClose={() => setNewPlace(null)} closeOnClick={false} closeButton={true} className="text-black" anchor="bottom" >
 
-                                    <div className="max-w-sm rounded overflow-hidden shadow-lg text-black">
-                                        <div className="w-full max-w-xs">
-                                            <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                                                <div className="mb-4">
-                                                    <label className="block text-gray-700 text-sm font-bold mb-2">
-                                                        Title
-                                                    </label>
-                                                    <input onChange={(e) => setNewTitle(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Title" />
-                                                </div>
-                                                <div className="mb-4">
-                                                    <label className="block text-gray-700 text-sm font-bold mb-2">
-                                                        Description
-                                                    </label>
-                                                    <input onChange={(e) => setNewDescription(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Tell me more about it" />
-                                                </div>
-                                                <div className='w-full flex justify-end'>
-                                                    <button className='rounded-2xl bg-green-400 w-14 h-6 text-white' type='submit'>Share</button>
-                                                </div>
-                                            </form>
+                                        <div className="max-w-sm rounded overflow-hidden shadow-lg text-black">
+                                            <div className="w-full max-w-xs">
+                                                <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                                                    <div className="mb-4">
+                                                        <label className="block text-gray-700 text-sm font-bold mb-2">
+                                                            Title
+                                                        </label>
+                                                        <input onChange={(e) => setNewTitle(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Title" />
+                                                    </div>
+                                                    <div className="mb-4">
+                                                        <label className="block text-gray-700 text-sm font-bold mb-2">
+                                                            Description
+                                                        </label>
+                                                        <input onChange={(e) => setNewDescription(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Tell me more about it" />
+                                                    </div>
+                                                    <div className='w-full flex justify-end'>
+                                                        <button className='rounded-2xl bg-green-400 w-14 h-6 text-white' type='submit'>Share</button>
+                                                    </div>
+                                                </form>
+                                            </div>
                                         </div>
-                                    </div>
 
-                                </Popup>}
-                        </>
+                                    </Popup>}
+                            </div>
 
-                        )
-                    })}
+                            )
+                        })}
 
-                </Map>
+                    </Map>
+                )}
             </div>
         </>
     );
