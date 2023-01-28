6dff4dfa341275ffebba01e6890af20498cabafd
Sidharthkp <sidharthkannan20@gmail.com>
2023-01-28 12:33:01 +0530

    IMage uploading in progress

--- src/components/pages/community/posts/createPost.tsx
+++ src/components/pages/community/posts/createPost.tsx
@@ -1,84 +1,42 @@
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { setCreateSwitchOff } from '../../../../redux/createPost';
-import 'firebase/auth'
-import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 const Modal = () => {
     const [description, setDescription] = useState('');
     const [image, setImage] = useState<File | null>(null);
+
+    const fileInput = useRef<HTMLInputElement>(null);
+
     const opened = useSelector((state: any) => state.showCreatePost.show);
     const dispatch = useDispatch();
-
-    // Get a reference to the storage service, which is used to create references in your storage bucket
-    const storage = getStorage();
-
-    // Create the file metadata
-    /** @type {any} */
-    const metadata = {
-        contentType: 'image/jpeg'
-    };
-
-    let storageRef: any;
-    let uploadTask: any;
-
-    // Upload file and metadata to the object 'images/mountains.jpg'
-    if (image) {
-        storageRef = ref(storage, 'images/' + image.name);
-        uploadTask = uploadBytesResumable(storageRef, image, metadata);
-    }
-
-    const user = localStorage.getItem('email')
 
     const closeModal = () => {
         dispatch(setCreateSwitchOff())
     }
 
-    const handleSubmit = () => {
-        if (image) {
-            // Listen for state changes, errors, and completion of the upload.
-            uploadTask.on('state_changed',
-                (snapshot: any) => {
-                    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                    console.log('Upload is ' + progress + '% done');
-                    switch (snapshot.state) {
-                        case 'paused':
-                            console.log('Upload is paused');
-                            break;
-                        case 'running':
-                            console.log('Upload is running');
-                            break;
-                    }
-                },
-                (error: any) => {
-                    // A full list of error codes is available at
-                    // https://firebase.google.com/docs/storage/web/handle-errors
-                    switch (error.code) {
-                        case 'storage/unauthorized':
-                            // User doesn't have permission to access the object
-                            break;
-                        case 'storage/canceled':
-                            // User canceled the upload
-                            break;
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
 
-                        // ...
+        if (fileInput.current && fileInput.current.files) {
 
-                        case 'storage/unknown':
-                            // Unknown error occurred, inspect error.serverResponse
-                            break;
-                    }
-                },
-                () => {
-                    // Upload completed successfully, now we can get the download URL
-                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                        console.log('File available at', downloadURL);
-                    });
-                }
-            );
-        } else {
-            console.log("Image not selected")
+
+            const post = {
+                name: localStorage.getItem('email'),
+                description: description,
+                productImage: fileInput.current.files[0] //.name,
+            };
+
+
+            axios
+                .post("http://localhost:3000/api/userPosts/post", post)
+                .then((res) => console.log(res.data))
+                .catch((err) => console.log(err));
+
+            alert(`Selected file - ${fileInput.current.files[0]}`);
+            console.log(fileInput.current.files[0]);
         }
     }
 
@@ -96,7 +54,7 @@ const Modal = () => {
                                     <button onClick={closeModal}><HighlightOffIcon className="text-sm text-blue-500 cursor-pointer" /></button>
                                 </div>
 
-                                <form>
+                                <form onSubmit={handleSubmit}>
                                     <div className='flex flex-col w-full py-5'>
                                         <div className='flex flex-row w-full justify-center'>
                                             <h1 className='text-black mb-5 text-4xl font-bold'>Create Your Post</h1>
@@ -115,12 +73,12 @@ const Modal = () => {
                                                 {image ?
                                                     <img src={image ? URL.createObjectURL(image) : ''} width="200px" height="200px" alt='' /> : null
                                                 }
-                                                <input onChange={(e: any) => setImage(e.target.files[0])} className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="user_avatar_help" id="user_avatar" type="file" />
+                                                <input ref={fileInput} name="postImage" onChange={(e: any) => setImage(e.target.files[0])} className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="user_avatar_help" id="user_avatar" type="file" />
                                             </div>
                                         </div>
                                         <div className="flex flex-row justify-center my-4">
                                             <div className="flex flex-col w-5/6">
-                                                <button onClick={handleSubmit} className="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                                                <button className="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
                                                     Post
                                                 </button>
                                             </div>
