5f387363fa773dd216fbe76fe5e0d1ce025f1d8d
Sidharthkp <sidharthkannan20@gmail.com>
2023-02-03 19:49:49 +0530

    COmmit changes

--- src/components/pages/profile/UserProfile.tsx
+++ src/components/pages/profile/UserProfile.tsx
@@ -4,13 +4,18 @@ import NavBar from "../../Navbar/User_side/Navbar"
 
 const Profile = () => {
     const [user, setUser] = useState<{ name: string } | null>(null);
+    const [community, setCommunity] = useState([]);
     useEffect(() => {
         const email = localStorage.getItem("email")
         axios.post("http://localhost:3000/api/profile/showProfile", { email })
             .then((res) => setUser(res.data)
             )
             .catch((err) => console.log(err));
-    }, [user])
+        axios.post("http://localhost:3000/api/profile/showCommunity", { email })
+            .then((res) => setCommunity(res.data)
+            )
+            .catch((err) => console.log(err));
+    }, [user, community])
     return (
         <div>
             <NavBar />
@@ -20,7 +25,7 @@ const Profile = () => {
                         <div className="rounded-b-2xl w-4/6 h-3/6">
                             <img className="rounded-b-2xl" src="https://avatars.githubusercontent.com/u/54587044?v=4" alt="" />
                         </div>
-                        <div className="flex flex-row items-center w-full">
+                        <div className="flex flex-row items-center w-full px-5">
                             <h1 className="mx-2 text-lg font-bold">Name:</h1>
                             <h1>{user && user.name ?
                                 <div>
@@ -31,19 +36,78 @@ const Profile = () => {
                                 :
                                 <div>
 
-                                    <h1>Please add your name</h1>
+                                    <h1 className="text-gray-500">Please add your name</h1>
 
                                 </div>
                             }</h1>
                         </div>
-                        <div className="flex flex-row items-center w-full">
+                        <div className="flex flex-row items-center w-full px-5">
                             <h1 className="mx-2 text-lg font-bold">Email:</h1>
                             <h1>{localStorage.getItem('email')}</h1>
                         </div>
                     </div>
                 </div>
-                <div className="w-4/6 h-full flex flex-row">
+                <div className="w-4/6 h-full flex flex-row justify-center p-5">
+                    <div className="m-5">
+                        <h1 className="font-bold text-lg mb-5">Your Community</h1>
+                        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                    <tr>
+                                        <th scope="col" className="px-6 py-3">
+                                            Community name
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                            Members
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                            Events
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                            Rides
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                            Action
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                            Action
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
 
+                                    {community.length > 0 && community.map((data: any) => {
+                                        return (
+                                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                    {data.groupName}
+                                                </th>
+                                                <td className="px-6 py-4">
+                                                    {data.members.length}
+                                                </td>
+                                                <td className="px-6 py-4">
+                                                    {data.events.length}
+                                                </td>
+                                                <td className="px-6 py-4">
+                                                    {data.rides.length}
+                                                </td>
+                                                <td className="px-6 py-4">
+                                                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                                </td>
+                                                <td className="px-6 py-4">
+                                                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })}
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+                    <div>
+
+                    </div>
                 </div>
             </div>
         </div>
