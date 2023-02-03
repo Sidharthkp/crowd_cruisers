8fce901dbe3b49c3c66732aaf88cae7cb2d09d0e
Sidharthkp <sidharthkannan20@gmail.com>
2023-02-03 18:30:58 +0530

    confirm

--- src/components/Authentication/Signup.tsx
+++ src/components/Authentication/Signup.tsx
@@ -49,7 +49,7 @@ const Signup = () => {
         signInWithPopup(auth, provider).then((data: any) => {
             localStorage.setItem("email", data.user.email)
             const uid = data.user.uid;
-            axios.post("http://localhost:3000/api/profile/addNew", { uid, email })
+            axios.post("http://localhost:3000/api/profile/addNew", { uid, email: data.user.email })
                 .then((res) => console.log(res)
                 )
                 .catch((err) => console.log(err));
