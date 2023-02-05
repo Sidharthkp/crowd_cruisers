2c64b8e2c6cd244f1931541eea69974f9ec369e3
Sidharthkp <sidharthkannan20@gmail.com>
2023-02-05 09:51:13 +0530

    commit

--- src/components/Pages/profile/UserProfile.tsx
+++ src/components/Pages/profile/UserProfile.tsx
@@ -146,7 +146,7 @@ const Profile = () => {
                                 <tbody>
 
                                     {/* {community.length > 0 && community.map((data: any) => { */}
-                                        return (
+                                        {/* return ( */}
                                             <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                                 <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                                     {/* {data.groupName} */}
@@ -205,7 +205,7 @@ const Profile = () => {
                                 <tbody>
 
                                     {/* {community.length > 0 && community.map((data: any) => { */}
-                                        return (
+                                        {/* return ( */}
                                             <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                                 <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                                     {/* {data.groupName} */}
