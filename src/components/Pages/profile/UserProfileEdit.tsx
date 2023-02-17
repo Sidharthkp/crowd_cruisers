702848dcb83cabee50fe37dc4d782919503c9dee
Sidharthkp <sidharthkannan20@gmail.com>
2023-02-17 22:14:34 +0530

    completed profile ui

--- src/components/Pages/profile/UserProfileEdit.tsx
+++ src/components/Pages/profile/UserProfileEdit.tsx
@@ -34,11 +34,11 @@ const UserProfileEdit = () => {
             {opened ? (
                 <>
                     <div
-                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                        className="justify-center w-full items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
                     >
                         <div className="relative w-screen my-6 mx-auto max-w-3xl">
                             {/*content*/}
-                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                            <div className="border-0 rounded-lg p-4 shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                                 <div className="flex w-full h-6 flex-row justify-end p-5">
                                     <button onClick={closeModal}><HighlightOffIcon className="text-sm text-blue-500 cursor-pointer" /></button>
                                 </div>
